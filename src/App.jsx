7f40bb3021e38bff00115bfdf9543e0bcec7d69c
Farzaan <syedfarzaanrafi@gmail.com>
2024-08-25 21:40:46 -0600

    Removed all CSS Styling. Also added more info about the movie.

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,50 @@ import './App.css';
 
 const RandomMoviePicker = () => {
     const [movie, setMovie] = useState({
+        title: 'Movie Title',
         imgSrc: 'assets/blank-image.jpg',
+        releaseYear: '',
+        genre: '',
+        director: '',
+        rating: '',
         description: 'Movie Description'
     });
 
     const handleButtonClick = () => {
-        // Implement your logic to get a random movie here
-        // For example, updating the movie state with a new image and description
         setMovie({
-            imgSrc: 'assets/random-movie-image.jpg',
-            description: 'This is a random movie description.'
+            title: 'Random Movie Title',
+            imgSrc: 'assets/blank-image.jpg',
+            releaseYear: 'NA',
+            genre: 'NA',
+            director: 'NA',
+            rating: 'NA',
+            description: 'This is a Random Movie Description'
         });
     };
 
     return (
-        <div className="top">
-            <h2 id="title">Random Movie Picker</h2>
-            <button id="button" onClick={handleButtonClick}>Get a Random Movie</button>
+        <div>
+            
+            <div class="top">
+                <h2 id="title">Random Movie Picker</h2>
+                <button id="button" onClick={handleButtonClick}>Get a Random Movie</button>
+            </div>
 
             <div className="movieDetails">
                 <img src={movie.imgSrc} id="movieImg" height="500" alt="Movie Poster" />
-                <h3 id="movieInfo">{movie.description}</h3>
+                <div>
+                    <h4 id="movieTitle">{movie.title}</h4>
+                    <p><strong>Release Year:</strong> <span id="releaseYear">{movie.releaseYear}</span></p>
+                    <p><strong>Genre:</strong> <span id="genre">{movie.genre}</span></p>
+                    <p><strong>Director:</strong> <span id="director">{movie.director}</span></p>
+                    <p><strong>Rating:</strong> <span id="rating">{movie.rating}</span></p>
+                    <p><strong>Description:</strong> <span id="description">{movie.description}</span></p>
+                </div>
+                    
             </div>
+
         </div>
+
     );
 };
 
