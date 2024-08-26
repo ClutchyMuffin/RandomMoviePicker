f8c4e2ceb61d7e94f48a330e4b8546b56cf19f18
Farzaan <syedfarzaanrafi@gmail.com>
2024-08-25 23:25:13 -0600

    Changed the styling of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from 'react';
+import defaultImage from './assets/blank-image.jpg';
 import './App.css';
 
 const RandomMoviePicker = () => {
     const [movie, setMovie] = useState({
         title: 'Movie Title',
-        imgSrc: 'assets/blank-image.jpg',
-        releaseYear: '',
-        genre: '',
-        director: '',
-        rating: '',
-        description: 'Movie Description'
+        imgSrc: defaultImage,
+        releaseYear: '<None>',
+        genre: '<None>',
+        director: '<None>',
+        rating: '<None>',
+        link: '<None>'
     });
 
     const handleButtonClick = () => {
         setMovie({
             title: 'Random Movie Title',
-            imgSrc: 'assets/blank-image.jpg',
-            releaseYear: 'NA',
-            genre: 'NA',
-            director: 'NA',
-            rating: 'NA',
-            description: 'This is a Random Movie Description'
+            imgSrc: defaultImage,
+            releaseYear: 'N/A',
+            genre: 'N/A',
+            director: 'N/A',
+            rating: 'N/A',
+            link: 'N/A'
         });
     };
 
@@ -34,15 +35,14 @@ const RandomMoviePicker = () => {
 
             <div className="movieDetails">
                 <img src={movie.imgSrc} id="movieImg" height="500" alt="Movie Poster" />
-                <div>
+                <div className="movieInfoContainer">
                     <h4 id="movieTitle">{movie.title}</h4>
-                    <p><strong>Release Year:</strong> <span id="releaseYear">{movie.releaseYear}</span></p>
-                    <p><strong>Genre:</strong> <span id="genre">{movie.genre}</span></p>
-                    <p><strong>Director:</strong> <span id="director">{movie.director}</span></p>
-                    <p><strong>Rating:</strong> <span id="rating">{movie.rating}</span></p>
-                    <p><strong>Description:</strong> <span id="description">{movie.description}</span></p>
+                    <p>Release Year: <span id="releaseYear">{movie.releaseYear}</span></p>
+                    <p>Genre: <span id="genre">{movie.genre}</span></p>
+                    <p>Director: <span id="director">{movie.director}</span></p>
+                    <p>Rating: <span id="rating">{movie.rating}</span></p>
+                    <p>Link: <span id="link">{movie.link}</span></p>
                 </div>
-                    
             </div>
 
         </div>
